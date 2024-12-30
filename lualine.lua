8a79749e8c26da726d75725f89914f562c8bfcc7
Natasha Wilhelm <ewilhelm31@gmail.com>
2024-12-30 06:25:29 -0500

    Create lualine.lua
    Lualine

---
+++ lualine.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_x, {
+      function()
+        return "   😄"
+      end,
+    })
+  end,
+}
